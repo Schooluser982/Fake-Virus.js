5e0f3cd954e05049146f9fb45749d39fb1a10927
Schooluser982 <gamerroblox29G@outlook.com>
2025-11-07 12:12:46 -0500

    Update Fake Virus.js

--- Fake Virus.js
+++ Fake Virus.js
@@ -1,3 +1,3 @@
---Go to this Google doc for the code, you can also edit the code to make it your own :)--
+--Go to this Google doc for the code, you can also edit the code by copying it into your own Google doc--
 
 --https://docs.google.com/document/d/1cNoa7LtcVNcXCf9RbCe3cW1p_Qd8CeZj31ql4QjRMIs/edit?tab=t.0--
